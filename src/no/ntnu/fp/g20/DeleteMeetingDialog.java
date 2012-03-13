915f450650fdf50ee291115a767cd81cc3ec4044
Jim Frode Hoff <jimfrodehoff@gmail.com>
2012-03-13 13:34:42 +0100

    Finished DeleteMeetingDialog and added NewInviteDialog

--- src/no/ntnu/fp/g20/DeleteMeetingDialog.java
+++ src/no/ntnu/fp/g20/DeleteMeetingDialog.java
@@ -1,37 +1,24 @@
 package no.ntnu.fp.g20;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-
 import javax.swing.*;
 
 public class DeleteMeetingDialog extends JOptionPane {
-	private String deleteMessage = "Du er i ferd med å slette møtet.\n"+
-								   "Fortsett?";
-	private String deleteTitle = "Slett møte?";
-	private String confirmButtonText = "Ja";
-	private String abortButtonText = "Avbryt";
+	private String deleteMessage = "You are about to delete this meeting.\n"+
+								   "Continue?";
+	private String deleteTitle = "Delete meeting?";
+	String[] buttons = { "Confirm", "Cancel"};
 	
-	/** 
-	 * Constructor.
-	 * Constructs a new dialogue.
-	 */
-	public DeleteMeetingDialog(JFrame parent)
-	{
-		super();
-	}
-
 	/**
-	 * 
 	 * @return Int, 0 for OK, 2 for CANCEL
 	 */
 	public int showDialog() {
-		return super.showConfirmDialog(getParent(), deleteMessage, deleteTitle, OK_CANCEL_OPTION);
+		return super.showOptionDialog(null, deleteMessage, deleteTitle,
+		        super.INFORMATION_MESSAGE, super.INFORMATION_MESSAGE, null, buttons, buttons[1]);
 	}
 	
 	public static void main(String[] args)
 	{
-		DeleteMeetingDialog dialog = new DeleteMeetingDialog(null);
+		DeleteMeetingDialog dialog = new DeleteMeetingDialog();
 		int input = dialog.showDialog();
 		String choice = (input==0 ? "OK" : "Cancel" ); 
 		System.out.println(choice);
