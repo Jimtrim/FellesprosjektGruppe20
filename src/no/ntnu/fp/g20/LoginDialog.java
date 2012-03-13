3621ba21dee6b7399114ea08d94d6a26ed1228e5
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2012-03-13 11:29:10 +0100

    Added a few methods and Javadoc to show how it is done.

--- src/no/ntnu/fp/g20/LoginDialog.java
+++ src/no/ntnu/fp/g20/LoginDialog.java
@@ -1,1 +1,30 @@
-// Here there will be a login dialogue.
+// Login Dialogue
+
+import javax.swing.*;
+
+/**
+ * Login dialogue class.
+ * @author Kristian Klomsten Skordal
+ */
+public class LoginDialog extends JDialog
+{
+	/** 
+	 * Constructor.
+	 * Constructs a new dialogue.
+	 */
+	public LoginDialog()
+	{
+	//	super("Calendar Login");
+	}
+
+	/**
+	 * Main class for testing.
+	 * @param args command line arguments.
+	 */
+	public static void main(String[] args)
+	{
+		LoginDialog dialog = new LoginDialog();
+		dialog.setVisible(true);
+	}
+}
+
