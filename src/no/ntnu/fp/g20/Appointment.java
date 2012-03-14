97bd21f8287499c9d386f79103b6bc12a1632f46
emil.oien.lunde@gmail.com <Emil@emiloien1337.(none)>
2012-03-14 14:52:49 +0100

    add Appointment

--- src/no/ntnu/fp/g20/Appointment.java
+++ src/no/ntnu/fp/g20/Appointment.java
@@ -1,18 +1,71 @@
 package no.ntnu.fp.g20;
 
 import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 
 public class Appointment {
 	
 	private int id;
-	private Date startTime;
+	private Date startDate;
+	private Time startTime;
 	private int duration;
 	private String location;
 	private Room room;
 	private ArrayList<User> listOfParticipants;
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
 	
+	public Time getStartTime() {
+		return startTime;
+	}
+	
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+	
+	public User[] getListOfParticipants() {
+		return listOfParticipants.toArray();
+	}
+
 	public void addParticipant(User participant) {
 		listOfParticipants.add(participant);
 	}
