7012a385049a9e6b3aebbd74fc96027158a2c98a
Erling W Eeg-Henriksen <erlweh@underdusken.no>
2012-03-21 11:31:34 +0100

    Made sortable list models for Appointments, Users and Rooms.

---
+++ src/no/ntnu/fp/g20/model/SortableUserListModel.java
@@ -0,0 +1,97 @@
+package no.ntnu.fp.g20.model;
+
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+
+public class SortableUserListModel extends DefaultListModel{
+	
+	private boolean isValidInsertion(Object o){
+		if (!(o instanceof User)){
+			System.err.println("Tried to add non-User object to SortableRoomListModel. Cancelled.");
+			return false;
+		}
+		return true;
+	}
+	public void addElement(Object element){
+		if (isValidInsertion(element)) return;
+		super.addElement(element);
+	}
+	public void add(int index, Object element){
+		if (isValidInsertion(element)) return;
+		super.add(index, element);
+	}
+	public void insertElementAt(Object obj, int index){
+		if (isValidInsertion(obj)) return;
+		super.insertElementAt(obj, index);
+	}
+	public Object set(int index, Object element){
+		if (isValidInsertion(element)) return getElementAt(index);
+		return super.set(index, element);
+	}
+	public void setElementAt(Object obj, int index){
+		if (isValidInsertion(obj)) return;
+		super.setElementAt(obj, index);
+	}
+	
+	public void sortByLastName(){
+		if (this.size()<2){ return; }
+		SortableUserListModel sorted = mergeSortByLastName(this);
+		this.clear();
+		for (int i=0; i<sorted.size(); i++){
+			this.addElement(sorted.get(i));
+		}
+	}
+
+	private static SortableUserListModel mergeSortByLastName(SortableUserListModel list) {
+		int mid = (int) list.size()/2;
+		SortableUserListModel p0 = new SortableUserListModel();
+		SortableUserListModel p1 = new SortableUserListModel();
+		//Splits:
+		for (int i=0; i<mid; i++){
+			p0.addElement(list.get(i));
+		}
+		for (int i=mid; i<list.size(); i++){
+			p1.addElement(list.get(i));
+		}
+		//Sorts halves:
+		p0 = mergeSortByLastName(p0);
+		p1 = mergeSortByLastName(p1);
+		//Merges and returns:
+		return mergeByLastName(p0,p1);
+	}
+	private static SortableUserListModel mergeByLastName(SortableUserListModel p0, SortableUserListModel p1){
+		SortableUserListModel merged = new SortableUserListModel();
+		int i=0;
+		int j=0;
+		//Merging:
+		while (i<p0.size() && j<p1.size()){
+			String p0_lastName = ((User)p0.getElementAt(i)).getLastName();
+			String p1_lastName = ((User)p1.getElementAt(i)).getLastName();
+			if (p0_lastName.compareToIgnoreCase(p1_lastName) < 0){
+				merged.addElement(p0.getElementAt(i++));
+			} else {
+				merged.addElement(p1.getElementAt(j++));
+			}
+		}
+		//Adding rest once one part is empty:
+		while (i<p0.size()){
+			merged.addElement(i++);
+		}
+		while (j<p1.size()){
+			merged.addElement(j++);
+		}
+		//Returns merged result:
+		return merged;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
