0870d84467d6959bacaa67bb4002f7c3f309df61
Kjetil <kjsletten@gmail.com>
2012-03-22 14:01:46 +0100

    Added the week.java

---
+++ src/no/ntnu/fp/g20/View.java
@@ -0,0 +1,18 @@
+package no.ntnu.fp.g20;
+
+import javax.swing.JFrame;
+
+import no.ntnu.fp.g20.database.Database;
+
+public class View extends JFrame {
+	private Database model;
+	
+	public View(Database model){
+		this.model = model;
+		
+		//Create the panels
+		
+		
+	}
+
+}
---
+++ src/no/ntnu/fp/g20/model/CalendarModel.java
@@ -0,0 +1,65 @@
+package no.ntnu.fp.g20.model;
+
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import javax.swing.table.AbstractTableModel;
+
+import no.ntnu.fp.g20.AppointmentListener;
+
+public class CalendarModel extends AbstractTableModel implements AppointmentListener {
+	private Appointment[12][7] appointments;
+	private User user;
+	private PropertyChangeSupport pcs;
+	
+	public CalendarModel(){
+		pcs = new PropertyChangeSupport(this);
+		
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void appointmentAdded(Appointment appointment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void appointmentUpdated(Appointment appointment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void appointmentDeleted(Appointment appointment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
