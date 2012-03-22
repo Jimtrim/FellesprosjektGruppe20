62ad1f3e58753615458a87fa0cb07d500d3ad7d1
Kristian Klomsten Skordal <kristian.skordal@gmail.com>
2012-03-22 10:42:14 +0100

    Changed commands to strings, see rest of commit message for reasoning
    Strings are more readable, that is the first and most important reason :-)
    As for string comparisons vs. integer comparisons, ie. one comparison
    of an integer, n comparisons for a string, please remember that converting
    a string into an integer is an operation that involves addition and multiplication,
    where multiplication is an expensive operation for most processors.
    
    All in all, the performance benefit of using integers is mostly killed
    by having to convert the integers from strings before the comparisons.
    
    :-)

--- src/no/ntnu/fp/g20/CalendarProtocol.java
+++ src/no/ntnu/fp/g20/CalendarProtocol.java
@@ -2,43 +2,43 @@ package no.ntnu.fp.g20;
 
 public class CalendarProtocol {
 	/* CLIENT CODES */
-	public final static int USER_LOGIN 		= 10;
-	public final static int USER_LOGOUT 	= 19;
-	
-	public final static int APPOINTMENT_CREATE = 20;
-	public final static int APPOINTMENT_UPDATE = 21;
-	public final static int APPOINTMENT_DELETE = 29;
+	public final static String CMD_LOGIN  = "LOGIN";
+	public final static String CMD_LOGOUT = "LOGOUT";
+g
+	public final static String CMD_APPOINTMENT_CREATE = "APPT CREATE";
+	public final static String CMD_APPOINTMENT_UPDATE = "APPT UPDATE";
+	public final static String CMD_APPOINTMENT_DELETE = "APPT DELETE";
 
-	public final static int ROOM_CREATE 	= 30;
-	public final static int ROOM_RESERVE 	= 31;
-	public final static int ROOM_UNRESERVE 	= 32;
-	public final static int ROOM_DELETE 	= 39;
+	public final static String CMD_ROOM_CREATE 	= "ROOM CREATE";
+	public final static String CMD_ROOM_RESERVE 	= "ROOM RES";
+	public final static String CMD_ROOM_UNRESERVE 	= "ROOM UNRES";
+	public final static String CMD_ROOM_DELETE 	= "ROOM DEL";
+
+	public final static String CMD_REQUEST_UPDATE 	= "UPDATE";
 	
-	public final static int GENERAL_REQUEST_UPDATE 	= 91;
+	/* Status codes */
+	public final static int STATUS_LOGIN_SUCCESS = 100;
+	public final static int STATUS_LOGIN_ERROR = 109;
+	public final static int STATUS_LOGOUT_SUCCESS = 190;
+	public final static int STATUS_LOGOUT_ERROR = 199;
 	
-	/* SERVER CODES */
-	public final static int SERVER_LOGIN_SUCCESS = 100;
-	public final static int SERVER_LOGIN_ERROR = 109;
-	public final static int SERVER_LOGOUT_SUCCESS = 190;
-	public final static int SERVER_LOGOUT_ERROR = 199;
+	public final static int STATUS_APPOINTMENT_CREATE_SUCCESS = 200;
+	public final static int STATUS_APPOINTMENT_CREATE_ERROR = 209;
+	public final static int STATUS_APPOINTMENT_UPDATE_SUCCESS = 210;
+	public final static int STATUS_APPOINTMENT_UPDATE_ERROR = 219;
+	public final static int STATUS_APPOINTMENT_DELETE_SUCCESS = 290;
+	public final static int STATUS_APPOINTMENT_DELETE_ERROR = 299;
 	
-	public final static int SERVER_APPOINTMENT_CREATE_SUCCESS = 200;
-	public final static int SERVER_APPOINTMENT_CREATE_ERROR = 209;
-	public final static int SERVER_APPOINTMENT_UPDATE_SUCCESS = 210;
-	public final static int SERVER_APPOINTMENT_UPDATE_ERROR = 219;
-	public final static int SERVER_APPOINTMENT_DELETE_SUCCESS = 290;
-	public final static int SERVER_APPOINTMENT_DELETE_ERROR = 299;
+	public final static int STATUS_ROOM_CREATE_SUCCESS = 300;
+	public final static int STATUS_ROOM_CREATE_ERROR = 309;
+	public final static int STATUS_ROOM_RESERVE_SUCCESS = 310;
+	public final static int STATUS_ROOM_RESERVE_ERROR = 319;
+	public final static int STATUS_ROOM_UNRESERVE_SUCCESS = 320;
+	public final static int STATUS_ROOM_UNRESERVE_ERROR = 329;
+	public final static int STATUS_ROOM_DELETE_SUCCESS = 390;
+	public final static int STATUS_ROOM_DELETE_ERROR = 391;
 	
-	public final static int SERVER_ROOM_CREATE_SUCCESS = 300;
-	public final static int SERVER_ROOM_CREATE_ERROR = 309;
-	public final static int SERVER_ROOM_RESERVE_SUCCESS = 310;
-	public final static int SERVER_ROOM_RESERVE_ERROR = 319;
-	public final static int SERVER_ROOM_UNRESERVE_SUCCESS = 320;
-	public final static int SERVER_ROOM_UNRESERVE_ERROR = 329;
-	public final static int SERVER_ROOM_DELETE_SUCCESS = 390;
-	public final static int SERVER_ROOM_DELETE_ERROR = 391;
-	
-	public final static int SERVER_GENERAL_NEW_UPDATES = 910;
-	public final static int SERVER_GENERAL_NO_UPDATES = 911;
-	public final static int SERVER_GENERAL_REQUEST_ERROR = 919;
+	public final static int STATUS_GENERAL_NEW_UPDATES = 910;
+	public final static int STATUS_GENERAL_NO_UPDATES = 911;
+	public final static int STATUS_GENERAL_REQUEST_ERROR = 919;
 }
